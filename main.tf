90c566396e2d7f98e1c4d1c429edd38dd5d9e24e
D1NaR1us <Dinar_1994@live.com>
2022-02-15 00:51:47 +0300

    allow http on hosts

--- main.tf
+++ main.tf
@@ -1,23 +1,27 @@
 provider "google" {
   project     = "involuted-earth-341207"
-  region      = "us-west1"
-  zone        = "us-west1-b"
+  region      = "europe-west1"
+  zone        = "europe-west1-b"
 }
 
-resource "google_compute_instance" "myserver" {
-  name         = "amsiblehost-${count.index}"
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance-${count.index + 1}"
   machine_type = "f1-micro"
   count = 2
+  tags = ["http-server"] 
+
   boot_disk {
     initialize_params {
       image = "centos-cloud/centos-7"
     }
   }
+
   network_interface {
     network = "default"
-    access_config {}
+    access_config {
+    }
   }
   metadata = {
-    sshKeys = "Dinarius:${file(var.ssh_public_key)}"
+   sshKeys = "Dinarius:${file(var.ssh_public_key)}"
   }
 }
