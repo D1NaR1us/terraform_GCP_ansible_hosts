bd2fb8d4a7f428e1bf084e08fc7c5c5f4bf0259f
D1NaR1us <Dinar_1994@live.com>
2022-02-15 01:06:59 +0300

    changed output file for hosts

--- output.tf
+++ output.tf
@@ -1,14 +1,14 @@
 output "external_ip" {
-    value = "${google_compute_instance.myserver[*].network_interface.0.access_config.0.nat_ip}"
+    value = "${google_compute_instance.vm_instance[*].network_interface.0.access_config.0.nat_ip}"
 }
 
 output "instance_id" {
-    value = "${google_compute_instance.myserver[*].name}"
+    value = "${google_compute_instance.vm_instance[*].name}"
 }
 output "ansible_host" {
     value = formatlist(
         "%v ansible_ssh_host=%v",
-            split(",","${join(",",google_compute_instance.myserver[*].name)}"),
-            split(",","${join(",",google_compute_instance.myserver[*].network_interface.0.access_config.0.nat_ip)}")
+            split(",","${join(",",google_compute_instance.vm_instance[*].name)}"),
+            split(",","${join(",",google_compute_instance.vm_instance[*].network_interface.0.access_config.0.nat_ip)}")
         )
 }
