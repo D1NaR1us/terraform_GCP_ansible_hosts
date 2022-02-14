752879cd71fba9a23ef38803dee6629dc9acf2af
D1NaR1us <Dinar_1994@live.com>
2022-02-14 15:39:29 +0300

    init and push firsts files

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "google" {
+  project     = "involuted-earth-341207"
+  region      = "us-west1"
+  zone        = "us-west1-b"
+}
+
+resource "google_compute_instance" "myserver" {
+  name         = "amsiblehost-${count.index}"
+  machine_type = "f1-micro"
+  count = 2
+  boot_disk {
+    initialize_params {
+      image = "centos-cloud/centos-7"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+  metadata = {
+    sshKeys = "Dinarius:${file(var.ssh_public_key)}"
+  }
+}
---
+++ output.tf
@@ -0,0 +1,14 @@
+output "external_ip" {
+    value = "${google_compute_instance.myserver[*].network_interface.0.access_config.0.nat_ip}"
+}
+
+output "instance_id" {
+    value = "${google_compute_instance.myserver[*].name}"
+}
+output "ansible_host" {
+    value = formatlist(
+        "%v ansible_ssh_host=%v",
+            split(",","${join(",",google_compute_instance.myserver[*].name)}"),
+            split(",","${join(",",google_compute_instance.myserver[*].network_interface.0.access_config.0.nat_ip)}")
+        )
+}
